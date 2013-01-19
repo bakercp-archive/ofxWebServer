f792354413127ae27cfc52408ee32b00d9c46dfb
Christopher Baker <me@christopherbaker.net>
2013-01-18 19:02:00 -0600

    Basic Websocket example.

---
+++ example-websocket/src/testApp.cpp
@@ -0,0 +1,32 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::exit() {
+    server->stop();
+    delete server;
+}
+
+//--------------------------------------------------------------
+void testApp::setup() {
+    ofSetFrameRate(30);
+    ofSetLogLevel(OF_LOG_VERBOSE);
+
+//    logo.loadImage("DocumentRoot/img/openFrameworks.png");
+    
+    server = new ofxWebServerBasic(8080,"DocumentRoot");
+
+    server->addRoute(ofxWebServerWebSocketRoute::Instance());
+    
+    server->start();
+
+    ofLaunchBrowser(server->getURL());
+}
+
+//--------------------------------------------------------------
+void testApp::draw() {
+    ofBackground(255);
+    ofSetColor(255);
+//    logo.draw(0,0,114,60);
+}
+
+
